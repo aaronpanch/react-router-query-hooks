54bdf69b9dd419fa50d18d6d47d2bc26164df028
Aaron Panchal <aaron.panchal@gmail.com>
2020-03-24 14:42:00 -0700

    Add entrypoint

---
+++ index.js
@@ -0,0 +1,3 @@
+export { default as useQueryString } from "src/useQueryString";
+export { default as useLocationWithQuery } from "src/useLocationWithQuery";
+export { default as useHistoryWithQuery } from "src/useHistoryWithQuery";
